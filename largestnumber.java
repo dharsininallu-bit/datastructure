979aae750b769ea125864949ab51fc466feb0ef9
DHARSHINI PRIYA <dharsininallu@gmail.com>
2026-06-18 15:22:11 +0530

    Create largestnumber.java

---
+++ largestnumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main{
+ public static void main (String[] args) 
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=in.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>max)
+            {
+                max=a[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
